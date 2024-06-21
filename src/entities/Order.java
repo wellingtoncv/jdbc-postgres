eb1d9744bdd717fe82afc1c9f93c13f333650762
Wellingtoncvs <wellington.santos@addasoftwares.com>
2024-06-20 22:26:50 -0300

    Jdbc + Postgres implementado

---
+++ src/entities/Order.java
@@ -0,0 +1,84 @@
+package entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private Double latitude;
+	private Double longitude;
+	private Instant moment;
+	private OrderStatus status;
+	
+	private List<Product> Products = new ArrayList<>();
+	
+	
+	public Order() {
+		
+	}
+
+	public Order(Long id, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public List<Product> getProducts() {
+		return Products;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", latitude=" + latitude + ", longitude=" + longitude + ", moment=" + moment
+				+ ", status=" + status + "]";
+	}
+	
+
+}
